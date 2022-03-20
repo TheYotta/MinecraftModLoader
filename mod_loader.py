a4298cab13fd18c41982b5b1a1e0b8ed73443169
TheYotta <82402768+TheYotta@users.noreply.github.com>
2022-03-20 19:49:39 +0100

    Add files via upload

---
+++ mod_loader.py
@@ -0,0 +1,27 @@
+import os
+from colored import fg
+
+while True:
+    current_mod = ""
+    dirs_cleared = []
+    text_f = open('modpacks.txt', "r")
+    mods = text_f.read().strip().split(",")
+    dirs = os.listdir()
+    for dir in dirs:
+        a = dir.split(".")
+        if len(a) == 1 and dir != "mods":
+            dirs_cleared.append(a[0])
+
+    for mod in mods:
+        if mod not in dirs_cleared:
+            current_mod = mod
+
+    wanted_mod = input(fg("yellow")+f"WANTED MOD (CURRENT - {current_mod} || OPTIONS - {dirs_cleared}):")
+
+    if wanted_mod in dirs_cleared:
+        if current_mod != "":
+            os.rename("mods", current_mod)
+        os.rename(wanted_mod, "mods")
+        print(fg("green")+f"LOADED [{wanted_mod}]")
+    if wanted_mod not in dirs_cleared:
+        print(fg("red")+"THIS MOD ISN'T DEFINED (modpacks.txt)")
